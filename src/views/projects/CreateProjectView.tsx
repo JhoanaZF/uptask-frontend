0db51b566d3f51bc9e4975ab720a9b97cff9c964
jhoana <jhoanazamora2@gmail.com>
2024-12-21 01:50:40 -0500

    pagina para crear proyectos

---
+++ src/views/projects/CreateProjectView.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+
+const CreateProjectView = () => {
+  return (
+    <>
+      <h1 className="text-5xl font-black">Crear Poyecto</h1>
+      <p className="text-2xl font-light text-gray-500 mt-5">LLena el siguiente formulario para crear un proyecto</p>
+      <nav className="my-5">
+        <Link
+          to={'/'}
+          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors">
+          Volver a Poyectos
+        </Link>
+      </nav>
+    </>
+  )
+}
+
+export default CreateProjectView
